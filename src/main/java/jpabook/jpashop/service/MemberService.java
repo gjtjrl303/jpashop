cee3b262de575f0b21fc91e9b486aa8a50213999
gjtjrl303 <gjtjrl303@naver.com>
2025-03-01 17:04:33 +0900

    feat: 회원 도메인 개발

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -36,7 +36,7 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    public Member findOne(Long id) {
-        return memberRepository.findOne(id);
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
     }
 }
